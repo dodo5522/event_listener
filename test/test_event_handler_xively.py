8a52c92c28213d4b4c758e5bcda2ac53cbe04428
Takashi Ando <dodo5522@gmail.com>
2016-10-02 00:28:04 +0900

    Xively送信時例外発生時の動作を検証するテストコード追加

--- test/test_event_handler_xively.py
+++ test/test_event_handler_xively.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import sys
+import time
 import unittest
 from datetime import datetime
 from event_listener.handler import XivelyEventHandler
@@ -76,6 +78,48 @@ class TestXivelyEventHandler(unittest.TestCase):
 
         self.assertEqual(2, len(client.datastreams))
         client.update.assert_called_once_with()
+
+    @patch("event_listener.handler.xively.XivelyAPIClient", autospec=True)
+    def test_post_data_failed_with_exception(self, mocked_api_client):
+        class ConnectionError(Exception):
+            pass
+
+        client = MagicMock()
+        client.update = MagicMock(return_value=None)
+        api = MagicMock()
+        api.feeds = MagicMock()
+        api.feeds.get = MagicMock(return_value=client)
+        mocked_api_client.side_effect = ConnectionError
+
+        xively_handler = XivelyEventHandler(
+            api_key="dummy_api_key",
+            feed_key="dummy_feed_key")
+
+        data = {}
+        data['source'] = 'solar'
+        data['at'] = datetime.now()
+        data['data'] = {
+            'Array Current': {'group': 'Array', 'unit': 'A', 'value': 1.4},
+            'Array Voltage': {'group': 'Array', 'unit': 'V', 'value': 53.41}}
+
+        xively_handler.start()
+        xively_handler.put_q(data)
+        xively_handler.join_q()
+
+        # ConnectionError発生時は１秒待って再送を３回繰り返す
+        time.sleep(5)
+
+        xively_handler.stop()
+        xively_handler.join()
+
+        self.assertEqual(mocked_api_client.call_count, 3)
+
+        if sys.version_info[:3] >= (3, 5, 0):
+            api.feeds.get.assert_not_called("dummy_feed_key")
+            client.update.assert_not_called()
+        else:
+            self.assertEqual(api.feeds.get.call_count, 0)
+            self.assertEqual(client.update.call_count, 0)
 
 
 if __name__ == "__main__":
